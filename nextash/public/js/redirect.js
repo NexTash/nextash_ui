c77bff8222822e4124a35fbe635f9785aa6d376a
Faisal Zahoor <faisal-zahoor>
2022-08-05 05:23:31 +0500

    fixes

--- nextash/public/js/redirect.js
+++
@@ -1,6 +0,0 @@
-const location_host = window.location.hostname
-
-if (location_host == "erpnextpk.com"){
-    alert("application domain has been changed, you will redirected to new domain")
-    window.location = "https://app.nextash.com"
-}
