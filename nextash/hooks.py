a884974a3518431cd3394577e83c26f70659a1c4
rizwanpak <rizwan.shahid@havenir.com>
2022-09-19 12:43:42 +0500

    feat(nextash checkin updated):

--- nextash/hooks.py
+++ nextash/hooks.py
@@ -46,8 +46,8 @@ doctype_js = {
 # doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
 # doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
-on_session_creation = 'nextash.events.employee_checkin.employee_checkin'
-on_logout='nextash.events.employee_checkin.employee_checkout'
+on_session_creation = 'nextash.nextash.events.employee_checkin.employee_checkin'
+on_logout='nextash.nextash.events.employee_checkin.employee_checkout'
 # Home Pages
 # ----------
 
@@ -146,7 +146,7 @@ fixtures = [
 # ------------------------------
 #
 # override_whitelisted_methods = {
-# 	"frappe.desk.doctype.event.event.get_events": "nextash.event.get_events"
+# 	"frappe.desk.doctype.event.event.get_events": "nextash.nextash.event.get_events"
 # }
 #
 # each overriding function accepts a `data` argument;
